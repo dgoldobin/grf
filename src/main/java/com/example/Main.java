eace95dba32bd3be22d901a229f603e9c89dab7b
Dmitriy A. Goldobin <gold@office.ems.chel.su>
2017-06-28 17:16:07 +0500

    yyy

--- src/main/java/com/example/Main.java
+++ src/main/java/com/example/Main.java
@@ -6,11 +6,13 @@ import org.slf4j.LoggerFactory;
 import java.awt.*;
 import java.awt.event.*;
 import java.awt.image.*;
-import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.List;
 import java.util.stream.Collectors;
+
+import java.io.*;
+
 
 public class Main {
     private static final Logger log = LoggerFactory.getLogger(Main.class);
@@ -18,11 +20,13 @@ public class Main {
     private static Image curImage = null;
 
     public static void main(String[] args) throws IOException, InterruptedException {
+        System.getProperties().setProperty("sun.java2d.opengl", "true");        
+
+
         List<GrfFile> files = Files.walk(Paths.get(args[0]))
                 .filter(path -> path.toString().toLowerCase().endsWith(".grf"))
                 .map(path -> new GrfFile(path.toFile()))
                 .collect(Collectors.toList());
-
         log.info("loaded {} files", files.size());
 
         final Frame frame = new Frame();
@@ -33,46 +37,61 @@ public class Main {
         });
         final Canvas canvas = new Canvas() {
             public void paint(Graphics g) {
+                long t = System.currentTimeMillis();
                 if (curImage == null) 
                     return;
-                long t = System.currentTimeMillis();
-                for (int i = 0; i < 1000; ++i)
-                    g.drawImage(curImage, 10, 10, null);
-                log.info("draw {} {}", curImage.getWidth(null) * curImage.getHeight(null), System.currentTimeMillis() - t);
+                BufferStrategy bs = getBufferStrategy();
+                g = bs.getDrawGraphics();
+//for (int k = 0; k < 100; ++k)
+                for (int y = 0; y < 10; ++y)
+                    for (int x = 0; x < 10; ++x) {
+                        int px = 256 * x + 128 * (y % 2);
+                        int py = 64 * y;
+                        g.drawImage(curImage, px, py, null);
+                    }
+                g.dispose();
+                bs.show();
+                log.info("dr {}", System.currentTimeMillis() - t);
                 
             }
         };
-        canvas.setBackground(Color.BLUE);
         frame.add(canvas);
         canvas.setBounds(0, 0, 300, 300);
+        canvas.setBackground(Color.BLUE);
         frame.pack();
         frame.setVisible(true);
+        canvas.createBufferStrategy(2);
 
-        int[] px = new int[256*256];
-        for (int x = 0; x < 256; ++x)
-            for (int y = 0; y < 256; ++y)
-                px[y*256 + x] = y << 24 | x << 8;
-        BufferedImage img2 = new BufferedImage(256, 256, BufferedImage.TYPE_INT_ARGB_PRE);
-        img2.setRGB(0, 0, 256, 256, px, 0, 256);
-        curImage = img2;
+        int[] pix = new int[256*256];
+        for (int i = 0; i < pix.length; ++i)
+            pix[i] = i << 16;
+        BufferedImage img1 = new BufferedImage(256, 256, BufferedImage.TYPE_INT_ARGB);
+        img1.setRGB(0, 0, 256, 256, pix, 0, 256);
+        curImage = img1;
         canvas.repaint();
-        
         
         files.stream()
                 .peek(f -> log.info("process {}", f))
                 .flatMap(GrfFile::allItems)
                 .filter(GrfItem::isPicture)
-                .filter(i -> (i.flags & GrfItem.FLAG_ZOOM_1) != 0)
+                .filter(i -> (i.flags & GrfItem.FLAG_ZOOM_1) != 0 && (i.flags & GrfItem.FLAG_ALPHA) != 0)
                 .forEach(i -> {
                     int w = i.getBounds().width;
                     int h = i.getBounds().height;
                     BufferedImage img = new BufferedImage(w, h, BufferedImage.TYPE_INT_ARGB);
+                    int[] px = i.getPixels(null, false);
+                    int alCount = 0;
+                    for (int j = 0; j < w * h; ++j) {
+                        int al = (px[j] >> 24) & 255;
+                        if (al > 50 && al < 200)
+                            ++alCount;
+                    }
+                    if (alCount < 100)
+                        return;
                     img.setRGB(0, 0, w, h, i.getPixels(null, true), 0, w);
-                    VolatileImage v = canvas.createVolatileImage(w, h);
-                    v.createGraphics().drawImage(img, 0, 0, null);
-                    curImage = v;
+                    curImage = img;
                     canvas.repaint();
-                    try {Thread.sleep(100);} catch(Exception e) {};
+                    try {Thread.sleep(1000);} catch(Exception e) {};
                 });
 
         frame.dispose();
